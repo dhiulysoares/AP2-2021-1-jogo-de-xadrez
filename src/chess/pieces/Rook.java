652625d474b717c3dab25be8af7f921148e738a6
Rodrigo F. Coutinho <cvc.francisco@gmail.com>
2021-11-04 19:17:13 -0300

    add Torre

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -8,6 +8,8 @@ public class Rook extends ChessPiece {
     public Rook(Board board, Color color) {
         super(board, color);
     }
+    
+    //Torre
 
     @Override
     public String toString() {
